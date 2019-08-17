5f4d490adb8637c0d8c45887967d89069878a199
Christopher Dent <christopherdent01@gmail.com>
2019-08-17 22:31:30 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,17 +4,55 @@ class String
 
   def sentence?
     
+    if self.end_with?(".")
+      true 
+    else 
+      false 
+   end 
   end
 
   def question?
+    if self.end_with?("?")
+      true 
+    else 
+      false 
+    end 
 
   end
 
   def exclamation?
+    
+    if self.end_with?("!")
+      return true 
+    else 
+      false 
+    end 
 
   end
 
   def count_sentences
-
+    
+    
+    string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
+    array = string.split(/[^\w\s\,]/)
+    
+    ## now you just have to remove the empty string 
+    
+    
+    
+    array.each do |string|
+      if string.empty? 
+        array.reject 
+      end 
+      
+    end 
+    binding.pry 
+    
+    sentences = array.count 
+    
+    p sentences 
+   
+    
+  
   end
 end
